d6e705f00911d2af8d8cda317893ac5c1725d9eb
zhumingwei <zhumingwei@wallstreetcn.com>
2018-09-07 11:44:30 +0800

    支持ripple

--- app/src/main/java/com/zhumingwei/shapedrawable/MainActivity.kt
+++ app/src/main/java/com/zhumingwei/shapedrawable/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zhumingwei.shapedrawable
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,9 @@ class MainActivity : AppCompatActivity() {
         tv1.setShape(20f,Color.RED)
         tv2.setShape(30f,Color.RED,10f)
         tv2.setTextColor(Color.BLACK)
+
+        tv3.setRipple(20f,Color.RED)
+        tv3.setOnClickListener { Toast.makeText(this,"123",Toast.LENGTH_SHORT).show() }
+//        tv3.background = resources.getDrawable(R.drawable.a)
     }
 }
--- app/src/main/java/com/zhumingwei/shapedrawable/ShapeUtil.kt
+++ app/src/main/java/com/zhumingwei/shapedrawable/ShapeUtil.kt
@@ -1,8 +1,15 @@
 package com.zhumingwei.shapedrawable
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.graphics.RectF
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.RippleDrawable
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.RoundRectShape
+import android.os.Build
+import android.support.annotation.RequiresApi
 import android.view.View
 
 
@@ -13,7 +20,11 @@ import android.view.View
  */
 
 fun View.setShape(radio: Float, color: Int, strokeWidth: Float = 0f) {
+    var shapeDrawable = getShapeDrawable(radio,color,strokeWidth)
+    background = shapeDrawable
+}
 
+fun getShapeDrawable(radio: Float, color: Int, strokeWidth: Float = 0f):ShapeDrawable{
     val shape = if (strokeWidth == 0f) {
         RoundRectShape(floatArrayOf(radio, radio, radio, radio, radio, radio, radio, radio),
                 null,
@@ -30,6 +41,18 @@ fun View.setShape(radio: Float, color: Int, strokeWidth: Float = 0f) {
     }
     val shapeDrawable = ShapeDrawable(shape)
     shapeDrawable.paint.color = color
+    return shapeDrawable
+}
 
-    background = shapeDrawable
+fun View.setRipple(radio: Float, color: Int, strokeWidth: Float = 0f){
+
+    var rippleDrawable: Drawable? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        RippleDrawable(ColorStateList.valueOf(Color.GRAY),getShapeDrawable(radio,color,strokeWidth),null)
+    } else {
+        getShapeDrawable(radio,color,strokeWidth)
+    }
+
+
+    background = rippleDrawable
+
 }
